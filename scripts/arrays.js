985ebd892328eaffa3e8918bcbf9afd4783977d1
AmandaSolee <79324616+AmandaSolee@users.noreply.github.com>
2022-10-17 13:45:01 -0400

    Add content to cards

--- scripts/arrays.js
+++ scripts/arrays.js
@@ -4,3 +4,22 @@ console.log(data);
 // 2. maybe we only show those that match the search query?
 
 // 3. we update the result count and related summary info as we filter
+
+const courseToCard = ({prefix, number, title,  url, desc, prereqs, credits}) => {
+    const courseTemplate = `<div class="col">
+                <div class="card" style="width: 18rem;">
+                <h3 class="card-header">${title}</h3>
+                <div class="card-body">
+                    <h5 class="card-title">${prefix} ${number}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">${title}</h6>
+                    <p class="card-text">${desc}</p>
+                </div>
+                </div>
+            </div>`;
+  return courseTemplate;
+}
+
+
+const courseCards = data.items.map(courseToCard).forEach((c) => document.write(c));
+// console.log(courseCards);
+// document.write(courseCards.join(''));
--- scripts/data.js
+++ scripts/data.js
@@ -30,5 +30,32 @@ const data = {
       prereqs: [159],
       credits: 3,
     },
+    {
+      prefix: "CS",
+      number: 227,
+      title: "Discrete Structures I",
+      url: "https://catalog.jmu.edu/content.php?filter%5B27%5D=CS&filter%5B29%5D=&filter%5Bcourse_type%5D=-1&filter%5Bkeyword%5D=&filter%5B32%5D=1&filter%5Bcpage%5D=1&cur_cat_oid=50&expand=&navoid=2576&search_database=Filter#acalog_template_course_filter",
+      desc: "An introduction to discrete mathematical structures including functions, relations, sets, logic, matrices, elementary number theory, proof techniques, basics of counting, graphic theory, discrete probability, digital logic, finite state machines, integer and floating point representations.",
+      prereqs: [159],
+      credits: 3,
+    },
+    {
+      prefix: "CS",
+      number: 240,
+      title: "Algorithms and Data Structures",
+      url: "https://catalog.jmu.edu/content.php?filter%5B27%5D=CS&filter%5B29%5D=&filter%5Bcourse_type%5D=-1&filter%5Bkeyword%5D=&filter%5B32%5D=1&filter%5Bcpage%5D=1&cur_cat_oid=50&expand=&navoid=2576&search_database=Filter#acalog_template_course_filter",
+      desc: "Students learn how to implement stacks, queues, lists, sets and maps using arrays, linked lists, binary trees, heaps, binary search trees, balanced trees and hashing. Students study graph representations and algorithms. Students analyze recursive and non-recursive algorithms, illustrated by various searching and sorting algorithms. Additional topics may be included as time permits. You may only enroll in CS 240 at most twice.",
+      prereqs: [159, 227],
+      credits: 3,
+    },
+    {
+      prefix: "CS",
+      number: 261,
+      title: "Computer Systems I",
+      url: "https://catalog.jmu.edu/content.php?filter%5B27%5D=CS&filter%5B29%5D=&filter%5Bcourse_type%5D=-1&filter%5Bkeyword%5D=&filter%5B32%5D=1&filter%5Bcpage%5D=1&cur_cat_oid=50&expand=&navoid=2576&search_database=Filter#acalog_template_course_filter",
+      desc: "Introduction to the operation of modern interrupt-driven computer systems. Explores the representation of software and information in binary memory, the primary components of a CPU, multithreaded programming and basic interactions with an Operating System. You may only enroll in CS 261 at most twice.",
+      prereqs: [227],
+      credits: 3,
+    }
   ],
 };
